fd3e6ca24cc0abbadef6b828924be3268a4e8758
RomanDubinin <Roman230394@yandex.ru>
2016-03-11 00:14:23 +0500

    поправил баги, доделал составление выравнивания.

--- alignment.py
+++ alignment.py
@@ -21,10 +21,14 @@ def print_table(table):
 def similarity(nuc1, nuc2):
     if (nuc1 == nuc2):
         return 1
-    return 0
+    return -1
 
 def create_empty_alignment_table(n, m):
     table = [[Cell(0) for x in range(m)] for x in range(n)]
+    for i in range(1, n):
+        table[i][0].second_string_shift = True
+    for j in range(1,m):
+        table[0][j].first_string_shift = True
     return table
 
 
@@ -44,14 +48,44 @@ def create_alignment_table(seq1, seq2):
             if (value == match):
                 table[i][j].no_shift = True
             if (value == delete):
-                table[i][j].first_string_shift = True
-            if (value == incert):
                 table[i][j].second_string_shift = True
+            if (value == incert):
+                table[i][j].first_string_shift = True
 
     return table
 
+def get_alignments(sequense1, sequense2):
+    table = create_alignment_table(sequense1, sequense2)
+    queue = []
+    queue.append((len(table)-1, len(table[0])-1, "", ""))
+    res = []
 
+    while len(queue) != 0:
+        i, j, seq1, seq2 = queue.pop()
+        if i == 0 and j == 0:
+            res.append((seq1, seq2))
+            continue
+        if table[i][j].first_string_shift:
+            seq1_copy = "-" + seq1
+            seq2_copy = sequense2[j-1] + seq2
+            queue.append((i, j-1, seq1_copy, seq2_copy))
+        if table[i][j].second_string_shift:
+            seq2_copy = "-" + seq2
+            seq1_copy = sequense1[i-1] + seq1
+            queue.append((i-1, j, seq1_copy, seq2_copy))
+        if table[i][j].no_shift:
+            seq1_copy = sequense1[i-1] + seq1
+            seq2_copy = sequense2[j-1] + seq2
+            queue.append((i-1, j-1, seq1_copy, seq2_copy))
 
+    return res
 
-t = create_alignment_table("ATA", "AGTA")
-print_table(t)
+#t = create_alignment_table("cat", "atactgcat")
+#print_table(t)
+
+alignments = get_alignments("cat", "atactctat")
+
+for a in alignments:
+    print(a[0])
+    print(a[1])
+    print("_______")
