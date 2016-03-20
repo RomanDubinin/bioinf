05d591c22e0537822355f6aa0ffb24cdfd7f5802
RomanDubinin <Roman230394@yandex.ru>
2016-03-20 13:53:00 +0500

    вес белка

--- 1.py
+++ 1.py
@@ -84,13 +84,35 @@ def min_super_string(words):
 
     return super_string
 
+def protein_mass(protein):
+    weights = {
+        'A':   71.03711,
+        'C':   103.00919,
+        'D':   115.02694,
+        'E':   129.04259,
+        'F':   147.06841,
+        'G':   57.02146,
+        'H':   137.05891,
+        'I':   113.08406,
+        'K':   128.09496,
+        'L':   113.08406,
+        'M':   131.04049,
+        'N':   114.04293,
+        'P':   97.05276,
+        'Q':   128.05858,
+        'R':   156.10111,
+        'S':   87.03203,
+        'T':   101.04768,
+        'V':   99.06841,
+        'W':   186.07931,
+        'Y':   163.06333,
+    }
+
+    weight = 0
+    for amino in protein:
+        weight += weights[amino]
+    return weight
 
 data = ''''''
 
-data_dict = fasta_to_dict(data)
-data = [data_dict[key] for key in data_dict]
-
-super_str = min_super_string(data)
-
-print(super_str)
-
+print(protein_mass("SKADYEK"))
