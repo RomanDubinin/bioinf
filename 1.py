5ff2f2f11b60ce235614dce3577a7c21947382ed
RomanDubinin <Roman230394@yandex.ru>
2016-03-22 23:52:51 +0500

    количество различных последовательностей rna, которые могут составить данный белок

--- 1.py
+++ 1.py
@@ -1,4 +1,22 @@
 import re
+from collections import defaultdict
+
+rna_to_protein_dict = {"UUU": "F",    "CUU": "L", "AUU": "I", "GUU": "V",
+                       "UUC": "F",    "CUC": "L", "AUC": "I", "GUC": "V",
+                       "UUA": "L",    "CUA": "L", "AUA": "I", "GUA": "V",
+                       "UUG": "L",    "CUG": "L", "AUG": "M", "GUG": "V",
+                       "UCU": "S",    "CCU": "P", "ACU": "T", "GCU": "A",
+                       "UCC": "S",    "CCC": "P", "ACC": "T", "GCC": "A",
+                       "UCA": "S",    "CCA": "P", "ACA": "T", "GCA": "A",
+                       "UCG": "S",    "CCG": "P", "ACG": "T", "GCG": "A",
+                       "UAU": "Y",    "CAU": "H", "AAU": "N", "GAU": "D",
+                       "UAC": "Y",    "CAC": "H", "AAC": "N", "GAC": "D",
+                       "UAA": "Stop", "CAA": "Q", "AAA": "K", "GAA": "E",
+                       "UAG": "Stop", "CAG": "Q", "AAG": "K", "GAG": "E",
+                       "UGU": "C",    "CGU": "R", "AGU": "S", "GGU": "G",
+                       "UGC": "C",    "CGC": "R", "AGC": "S", "GGC": "G",
+                       "UGA": "Stop", "CGA": "R", "AGA": "R", "GGA": "G",
+                       "UGG": "W",    "CGG": "R", "AGG": "R", "GGG": "G"}
 
 def cg_content(str):
     dict = {"A":0,"C":0, "G":0, "T":0}
@@ -170,28 +188,12 @@ def dna_to_rna(dna):
     return dna.replace("T", "U")
 
 def rna_to_protein(rna):
-    rep = {"UUU": "F", "CUU": "L", "AUU": "I", "GUU": "V",
-           "UUC": "F", "CUC": "L", "AUC": "I", "GUC": "V",
-           "UUA": "L", "CUA": "L", "AUA": "I", "GUA": "V",
-           "UUG": "L", "CUG": "L", "AUG": "M", "GUG": "V",
-           "UCU": "S", "CCU": "P", "ACU": "T", "GCU": "A",
-           "UCC": "S", "CCC": "P", "ACC": "T", "GCC": "A",
-           "UCA": "S", "CCA": "P", "ACA": "T", "GCA": "A",
-           "UCG": "S", "CCG": "P", "ACG": "T", "GCG": "A",
-           "UAU": "Y", "CAU": "H", "AAU": "N", "GAU": "D",
-           "UAC": "Y", "CAC": "H", "AAC": "N", "GAC": "D",
-           "UAA": "Stop", "CAA": "Q", "AAA": "K", "GAA": "E",
-           "UAG": "Stop", "CAG": "Q", "AAG": "K", "GAG": "E",
-           "UGU": "C", "CGU": "R", "AGU": "S", "GGU": "G",
-           "UGC": "C", "CGC": "R", "AGC": "S", "GGC": "G",
-           "UGA": "Stop", "CGA": "R", "AGA": "R", "GGA": "G",
-           "UGG": "W", "CGG": "R", "AGG": "R", "GGG": "G"}
-
     res = ""
     for i in range(0, len(rna), 3):
-        res += rep[rna[i:i+3]]
+        res += rna_to_protein_dict[rna[i:i+3]]
 
     return res
+
 
 def rna_splicing(dna, introns):
     new_dna = dna
@@ -202,6 +204,18 @@ def rna_splicing(dna, introns):
     proteins = rna_to_protein(rna).replace("Stop", "")
     return proteins
 
+def number_of_different_rna_from_protien(protein):
+    res = 1
+
+    count_of_different_codons = defaultdict(int)
+    for codon in rna_to_protein_dict:
+        count_of_different_codons[rna_to_protein_dict[codon]] += 1
+
+    for letter in protein:
+        res = res * count_of_different_codons[letter]
+
+    return res * 3 # 4 Stop codons
+
 
 data = '''>Rosalind_10
 ATGGTCTACATAGCTGACAAACAGCACGTAGCAATCGGTCGAATCTCGAGAGGCATATGGTCACATGATCGGTCGAGCGTGTTTCAAAGTTTGCGCCTAG
@@ -210,9 +224,6 @@ ATCGGTCGAA
 >Rosalind_15
 ATCGGTCGAGCGTGT'''
 
-reads = fasta_to_list(data)
 
-#reads = [data_dict[key] for key in data_dict]
-
-print(rna_splicing(reads[0], reads[1:]))
+print(number_of_different_rna_from_protien("MLAGLFWTIEAGRPSQAEMDKYRSPCAPARADVMTSNHYKNCCIDWNQLCRHILCWFTWYWELTQSTIWFGYWGPSPPLCDIHQERGLLWTGQTHTPGLTGGTQKWYDIRKVHIFTMHMMGRQQCEVECTCVHDKPGGRDSCNSDLAFHFNSFHMEMACNGEHWFLRKWVYVFTEMPDREGMENIWVLAKTCNQYPVKSKPKGIRYTLQTRSCHPHLKEHIICYVRTMTICMQVNVTFNMYTFEVRYAFFTIEVGVEENRLWSVQAFCSCQHWQMMSWNDGAPWEPHNARMMHHGSFNLNYDGKMGGDCCFPQTMLSIPLAGESQKRAIHEPHDFVGHDIYMNGETRAMSPEYAWMSVPPVRWDECKHAFINYIGLRNSKVKFEYNFRAWKRVLHQHNEHLGWKSTTVICCDNHFEPNNCFLGCPTIWRKDDNRMINIYYYFWPRKLEYPEQWDVNIFNSNIPHWIMRLQYVWVTNIIPNHEWCQIARFKLLECEKNYDDNKWIWMKHHHGFRFRCVTFDSYDVDTLYHHNEECIICVLWRQMQHVECYTAPPSPPSYQKREDHSMNAVKAPCILNFLKHVATQVTIRYMHKRYMLNTEYAVHSSPTIRLQCAWQWNWNWLMDEHWVYLCISIAFQADMDKMLIGYEGWPSGAGFHMYIEFQEWELYRKVYYEVSTSTYYYQDHPVCLVYCTNKIMRSSAWLKQLTMQVHWTCPYIHQFRCYYPYGQCIDDMKMCFDPAKKYIKHGLPCYPQKYMHDRYKLICGQFMADDGNSQFEGLTAMVHVCGSHKDCLTQGKCKCNETQNLTLCLMWDTWHCQMYPDMMGISCICKYRPWEAKGRLVCVGYLETAKLRRYGFQKAVMMHLALSVGTHGQLEAMHPTEEHSEYYGPDWYEFKTGYEVVDCSAIWFIDTCDQNWARVKFPTMHQIYQWEYQLECPEFPTCARTMYIKVFCWVVIATQMSQKSDMGSDFPMRLDNKQHICYCRMTAQCQVQLSRNQ") % 1000000)
 
